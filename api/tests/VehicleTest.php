e51df59c81879a328408b2aac0a33444b3b89fad
Bartha Norbert <norbi@Barthas-MacBook-Pro.local>
2023-07-03 07:01:31 +0300

    Add one more unit test

--- api/tests/VehicleTest.php
+++ api/tests/VehicleTest.php
@@ -60,6 +60,48 @@ class VehicleTest extends TestCase
     }
 
     /**
+     * @dataProvider addMultipleTechnicalDetailProvider
+     */
+    public function testAddMultipleTechnicalDetail(
+        array $technicalDetails,
+        int $expectedResult
+    ): void {
+        foreach ($technicalDetails as $technicalDetail) {
+            $this->vehicle->addTechnicalDetail($technicalDetail);
+        }
+
+        $this->assertEquals($expectedResult, $this->vehicle->getTechnicalDetails()->count());
+    }
+
+    public function addMultipleTechnicalDetailProvider(): array
+    {
+        $technicalDetails = [
+            (new TechnicalDetail())->setName('name1')->setValue('value1'),
+            (new TechnicalDetail())->setName('name2')->setValue('value2'),
+            (new TechnicalDetail())->setName('name3')->setValue('value3'),
+            (new TechnicalDetail())->setName('name4')->setValue('value4'),
+            (new TechnicalDetail())->setName('name5')->setValue('value5'),
+            (new TechnicalDetail())->setName('name6')->setValue('value6'),
+            (new TechnicalDetail())->setName('name7')->setValue('value7'),
+            (new TechnicalDetail())->setName('name8')->setValue('value8'),
+            (new TechnicalDetail())->setName('name9')->setValue('value9'),
+            (new TechnicalDetail())->setName('name10')->setValue('value10'),
+            (new TechnicalDetail())->setName('name11')->setValue('value11'),
+            (new TechnicalDetail())->setName('name12')->setValue('value12'),
+            (new TechnicalDetail())->setName('name13')->setValue('value13'),
+            (new TechnicalDetail())->setName('name14')->setValue('value14'),
+            (new TechnicalDetail())->setName('name15')->setValue('value15'),
+        ];
+
+        return [
+            'Success' => [
+                'technicalDetails' => $technicalDetails,
+                'expectedResult' => 10,
+            ],
+        ];
+    }
+
+    /**
      * @dataProvider removeTechnicalDetailProvider
      */
     public function testRemoveTechnicalDetail(TechnicalDetail $technicalDetail, int $expectedResult): void
